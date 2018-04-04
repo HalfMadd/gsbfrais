c249fd8f5098e638cc414290d2d18686684e4135
Rémi LACUISSE <remi72@free.fr>
2018-04-04 17:22:11 +0200

    ^M

--- cSeDeconnecter.php
+++ cSeDeconnecter.php
@@ -1,1 +1,25 @@
- <?php  /**  * Script de contr�le et d'affichage du cas d'utilisation "Se d�connecter" * @package default * @todo  RAS */  $repInclude = './include/';  require($repInclude . "_init.inc.php");    deconnecterVisiteur() ;    header("Location:cSeConnecter.php");  ?>
+﻿ <?php  
+
+/** 
+
+ * Script de contr�le et d'affichage du cas d'utilisation "Se d�connecter"
+
+ * @package default
+
+ * @todo  RAS
+
+ */
+
+  $repInclude = './include/';
+
+  require($repInclude . "_init.inc.php");
+
+  
+
+  deconnecterVisiteur() ;  
+
+  header("Location:cSeConnecter.php");
+
+  
+
+?>
